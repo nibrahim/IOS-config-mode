1a676f828c16082f1cebfe043698980c6ca37982
Noufal Ibrahim <noufal@nibrahim.net.in>
2010-11-09 21:40:08 +0530

    Initial commit

---
+++ ios-config-addons.el
@@ -0,0 +1,73 @@
+;;; ios-config-addons.el
+;; Copyright (C) 2004 Noufal Ibrahim <nkv at hcoop period net>
+;;
+;; This program is not part of Gnu Emacs
+;;
+;; ios-config-addons.el is free software and a part of
+;; ios-config-mode.el; you can redistribute it and/or modify it under
+;; the terms of the GNU General Public License as published by the
+;; Free Software Foundation; either version 2 of the License, or (at
+;; your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program; if not, write to the Free Software
+;; Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+(defun ioscfg-add-command-to-interfaces (command &optional intf)
+  "Add COMMAND (if it's not already there) to interfaces that match regexp INTF. If INTF is null, work on all interfaces.
+   This requires the buffer to be indented properly."
+  (interactive "MCommand: \nMInterface: ") 
+  (save-excursion
+    (save-restriction
+      (widen)
+      (goto-char (point-min))
+      (while (progn
+	       (if (looking-at (concat "^[Ii]nterface " intf))
+		   (let ((block-end (ioscfg-find-interface-block (point))))
+		     (if (not (ioscfg-command-in-block-p command block-end))
+			 (save-excursion
+			   (goto-char (+ block-end 1))
+			   (insert " " command "\n")))))
+	       (not (= (forward-line) 1)))))))
+	
+
+(defun ioscfg-find-interface-block (pt)
+  (save-excursion 
+    (goto-char pt)
+    (forward-line)
+    (while (looking-at "^ .*")
+      (forward-line 1))
+    (- (point) 1)))
+      
+(defun ioscfg-command-in-block-p (cmd end)
+  (save-excursion
+    (search-forward-regexp cmd end t)))
+  
+(defun ioscfg-unshut-all-interfaces ()
+  "Removes the \"shutdown\" or \"shut\" command from all interfaces and adds a \"no shutdown\" instead."
+  (interactive)
+  (save-excursion
+    (goto-char (point-min))
+    (while (re-search-forward "^\\([ \t]*shut\\(down\\)?\\)$" nil t)
+      (beginning-of-line)
+      (kill-line 1)))
+  (ioscfg-add-command-to-interfaces "no shutdown"))
+
+(defun ioscfg-shut-all-interfaces ()
+  "Removes the \"no shutdown\" or \"no shut\" command from all interfaces and adds a \"shutdown\" instead."
+  (interactive)
+  (save-excursion
+    (goto-char (point-min))
+    (while (re-search-forward "^\\([ \t]*no shut\\(down\\)?\\)$" nil t)
+      (beginning-of-line)
+      (kill-line 1)))
+  (ioscfg-add-command-to-interfaces "shutdown"))
+				      
+(provide 'ios-config-addons)
+
+;;; ios-config-addons.el ends here
---
+++ ios-config-mode.el
@@ -0,0 +1,154 @@
+;;; ios-config-mode.el
+;; Copyright (C) 2004 Noufal Ibrahim <nkv at hcoop period net>
+;;
+;; This program is not part of Gnu Emacs
+;;
+;; ios-config-mode.el is free software; you can
+;; redistribute it and/or modify it under the terms of the GNU General
+;; Public License as published by the Free Software Foundation; either
+;; version 2 of the License, or (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program; if not, write to the Free Software
+;; Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+(defvar ios-config-mode-hook nil
+  "Hook called by \"ios-config-mode\"")
+
+(defvar ios-config-mode-map
+  (let 
+      ((ios-config-mode-map (make-keymap)))
+    (define-key ios-config-mode-map "\C-j" 'newline-and-indent)
+    ios-config-mode-map)
+  "Keymap for Cisco router configuration major mode")
+
+;; Font locking definitions. 
+(defvar ios-config-command-face 'ios-config-command-face "Face for basic router commands")
+(defvar ios-config-toplevel-face 'ios-config-toplevel-face "Face for top level commands")
+(defvar ios-config-no-face 'ios-config-no-face "Face for \"no\"")
+(defvar ios-config-ipadd-face 'ios-config-ipadd-face "Face for IP addresses")
+
+(defface ios-config-ipadd-face
+  '(
+    (((type tty) (class color)) (:foreground "yellow"))
+    (((type graphic) (class color)) (:foreground "LightGoldenrod"))
+    (t (:foreground "LightGoldenrod" ))
+    )
+  "Face for IP addresses")
+
+(defface ios-config-command-face 
+  '(
+    (((type tty) (class color)) (:foreground "cyan"))
+    (((type graphic) (class color)) (:foreground "cyan"))
+    (t (:foreground "cyan" ))
+    )
+  "Face for basic router commands")
+
+(defface ios-config-toplevel-face
+  '(
+    (((type tty) (class color)) (:foreground "blue"))
+    (((type graphic) (class color)) (:foreground "lightsteelblue"))
+    (t (:foreground "lightsteelblue" ))
+    )
+  "Face for basic router commands")
+
+(defface ios-config-no-face
+  '(
+    (t (:underline t))
+    )
+  "Face for \"no\"")
+
+
+;; (regexp-opt '("interface" "ip vrf" "controller" "class-map" "redundancy" "line" "policy-map" "router" "access-list" "route-map") t)
+;; (regexp-opt '("diagnostic" "hostname" "logging" "service" "alias" "snmp-server" "boot" "card" "vtp" "version" "enable") t)
+
+(defconst ios-config-font-lock-keywords
+  (list
+   '( "\\<\\(access-list\\|c\\(?:lass-map\\|ontroller\\)\\|i\\(?:nterface\\|p vrf\\)\\|line\\|policy-map\\|r\\(?:edundancy\\|oute\\(?:-map\\|r\\)\\)\\)\\>". ios-config-toplevel-face)
+   '( "\\<\\(alias\\|boot\\|card\\|diagnostic\\|^enable\\|hostname\\|logging\\|s\\(?:ervice\\|nmp-server\\)\\|v\\(?:ersion\\|tp\\)\\)\\>" . ios-config-command-face)
+   '("\\<\\(no\\)\\>" . ios-config-no-face)
+   '("\\<\\([0-9]\\{1,3\\}\\.[0-9]\\{1,3\\}\\.[0-9]\\{1,3\\}\\.[0-9]\\{1,3\\}\\)\\>" . ios-config-ipadd-face)
+   )
+  "Font locking definitions for cisco router mode")
+
+;; Imenu definitions. 
+(defvar ios-config-imenu-expression
+  '(
+    ("Interfaces"        "^[\t ]*interface *\\(.*\\)" 1)
+    ("VRFs"              "^ip vrf *\\(.*\\)" 1)
+    ("Controllers"       "^[\t ]*controller *\\(.*\\)" 1)
+    ("Routing protocols" "^router *\\(.*\\)" 1)
+    ("Class maps"        "^class-map *\\(.*\\)" 1)
+    ("Policy maps"       "^policy-map *\\(.*\\)" 1)
+    ))
+  
+;; Indentation definitions.
+(defun ios-config-indent-line ()
+  "Indent current line as cisco router config line"
+  (let ((indent0 "^interface\\|redundancy\\|^line\\|^ip vrf \\|^controller\\|^class-map\\|^policy-map\\|router\\|access-list\\|route-map")
+	(indent1 " *main-cpu\\| *class\\W"))
+    (beginning-of-line)
+    (let ((not-indented t)
+	  (cur-indent 0))
+      (cond ((or (bobp) (looking-at indent0) (looking-at "!")) ; Handles the indent0 and indent1 lines
+	     (setq not-indented nil
+		   cur-indent 0))
+	    ((looking-at indent1)
+	     (setq not-indented nil
+		   cur-indent 1)))
+      (save-excursion ; Indents regular lines depending on the block they're in.
+	(while not-indented
+	  (forward-line -1)
+	  (cond ((looking-at indent1)
+		 (setq cur-indent 2
+		       not-indented nil))
+		((looking-at indent0)
+		 (setq cur-indent 1
+		       not-indented nil))
+		((looking-at "!")
+		 (setq cur-indent 0
+		       not-indented nil))
+		((bobp) 
+		 (setq cur-indent 0
+		       not-indented nil)))))
+      (indent-line-to cur-indent))))
+
+
+;; Custom syntax table
+(defvar ios-config-mode-syntax-table (make-syntax-table) 
+  "Syntax table for cisco router mode")
+
+(modify-syntax-entry ?_ "w" ios-config-mode-syntax-table) ;All _'s are part of words. 
+(modify-syntax-entry ?- "w" ios-config-mode-syntax-table) ;All -'s are part of words. 
+(modify-syntax-entry ?! "<" ios-config-mode-syntax-table) ;All !'s start comments. 
+(modify-syntax-entry ?\n ">" ios-config-mode-syntax-table) ;All newlines end comments.
+(modify-syntax-entry ?\r ">" ios-config-mode-syntax-table) ;All linefeeds end comments.
+
+;; Entry point
+(defun ios-config-mode  ()
+  "Major mode for editing Cisco IOS (tm) configuration files"
+  (interactive)
+  (kill-all-local-variables)
+  (set-syntax-table ios-config-mode-syntax-table)
+  (use-local-map ios-config-mode-map)
+  (set (make-local-variable 'font-lock-defaults) '(ios-config-font-lock-keywords))
+  (set (make-local-variable 'indent-line-function) 'ios-config-indent-line)
+  (set (make-local-variable 'comment-start) "!")
+  (set (make-local-variable 'comment-start-skip) "\\(\\(^\\|[^\\\\\n]\\)\\(\\\\\\\\\\)*\\)!+ *")
+  (setq imenu-case-fold-search nil)  
+  (set (make-local-variable 'imenu-generic-expression) ios-config-imenu-expression)
+  (imenu-add-to-menubar "Imenu")
+  (setq major-mode 'ios-config-mode
+	mode-name "IOS configuration")
+  (run-hooks ios-config-mode-hook))
+
+(add-to-list 'auto-mode-alist '("\\.cfg\\'" . ios-config-mode))
+
+(provide 'ios-config-mode)
+
+;;; ios-config-mode.el ends here
